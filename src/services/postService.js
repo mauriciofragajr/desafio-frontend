ce30faebed9ef4b9d7226afdc800313ad07893da
Mauricio Fraga Jr <mauriciofragajr@gmail.com>
2018-07-03 01:43:50 -0300

    criando post service

---
+++ src/services/postService.js
@@ -0,0 +1,22 @@
+import api from './api';
+
+export default {
+    get(q = "", page = 1) {
+        return api.get(`/posts?page=${page}&q=${q}`);
+    },
+    getDetail(slug) {
+        return api.get(`/posts/${slug}`);
+    },
+    getByCategory(category_slug, page = 1) {
+        return api.get(`/posts/categories/${category_slug}?page=${page}`);
+    },
+    create(post) {
+        return api.post(`/posts`, post);
+    },
+    update(post) {
+        return api.delete(`/posts/${post.slug}`, post);
+    },
+    delete(post_slug) {
+        return api.delete(`/posts/${post_slug}`);
+    }
+}
